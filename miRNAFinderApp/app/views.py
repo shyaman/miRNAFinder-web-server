e614961de7df9ddbe27b826df29c45d43ec7ae67
shyaman <shayaman321@gmail.com>
2020-06-04 10:35:03 +0530

    results page

--- miRNAFinderApp/app/views.py
+++ miRNAFinderApp/app/views.py
@@ -2,19 +2,39 @@ from app import app, mlModel
 from flask import render_template
 from flask import request
 
-@app.route("/")
+@app.route("/",methods = ['GET'])
 def index():
     return render_template('index.html')
 
-@app.route("/resources")
+@app.route("/resources",methods = ['GET'])
 def resources():
     return render_template('resources.html')
 
-@app.route("/about-us")
+@app.route("/about-us",methods = ['GET'])
 def about_us():
     return render_template('about-us.html')
 
 @app.route("/predict", methods=['POST'])
 def predict():
-    data = request.form.get('sequence')
-    return render_template('index.html', prediction="It's a mirna")
+    # Get the file from post request
+    f = request.json
+    print(f)
+    # # Save the file to ./uploads
+    # basepath = os.path.dirname(__file__)
+    # file_path = os.path.join(
+    #     basepath, 'uploads', secure_filename(f.filename))
+    # f.save(file_path)
+
+    # # Make prediction
+    # preds = model_predict(file_path, model)
+
+    # # Process your result for human
+    # # pred_class = preds.argmax(axis=-1)            # Simple argmax
+    # pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
+    # result = str(pred_class[0][0][1])               # Convert to string
+    return "None"
+
+
+@app.route("/results",methods = ['POST','GET'])
+def results():
+    return render_template('results.html')
