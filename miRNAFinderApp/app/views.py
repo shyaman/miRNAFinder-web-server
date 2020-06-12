f466142ea530e6165413d81e449357c9a7fd0db7
shyaman <shayaman321@gmail.com>
2020-06-12 23:49:02 +0530

    update views and add resources

--- miRNAFinderApp/app/views.py
+++ miRNAFinderApp/app/views.py
@@ -28,12 +28,16 @@ def results():
     sessionID = mlModel.getSessID()
     return render_template('results.html',tables=[pred['pred'].to_html(classes='table', border=0, index=False, justify='inherit')],sID=sessionID)
 
-@app.route('/feat/<path:filename>', methods=['GET', 'POST'])
+@app.route('/feat/<path:filename>', methods=['GET'])
 def download_feat(filename):
     direc = mlModel.getDataDir() + '/' + filename
     return send_from_directory(directory=direc, filename='features.xlsx',as_attachment=True)
 
-@app.route('/pred/<path:filename>', methods=['GET', 'POST'])
+@app.route('/pred/<path:filename>', methods=['GET'])
 def download_pred(filename):
     direc = mlModel.getDataDir() + '/' + filename
     return send_from_directory(directory=direc, filename='predications.xlsx',as_attachment=True)
+
+@app.route('/resourcesdw/<path:filename>', methods=['GET'])
+def resourcesdw(filename):
+    return send_from_directory(directory='static/resources', filename=filename,as_attachment=True)
