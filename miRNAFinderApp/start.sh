89ba74b6f04edaba797b1bfffe616e76c902b9d4
shyaman <shayaman321@gmail.com>
2021-04-25 20:39:23 +0530

    move redis intall to docker

--- miRNAFinderApp/start.sh
+++ miRNAFinderApp/start.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 envsubst '\$PORT' < nginx.conf > /etc/nginx/nginx.conf
 service nginx start
-./run-redis.sh
 nohup celery -A app.celery_app worker -B -l info &> celery.log &
 uwsgi --ini app.ini
